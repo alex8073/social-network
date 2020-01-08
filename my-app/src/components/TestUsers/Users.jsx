5fc5249deb2c49b61523d73d2bc11eb4608558de
alex8073 <alexandr8073@gmail.com>
2020-01-08 22:02:47 +0300

    dev 54  life cycle methods, componentDidMount

--- my-app/src/components/TestUsers/Users.jsx
+++ my-app/src/components/TestUsers/Users.jsx
@@ -5,21 +5,14 @@ import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
         });
     }
 
-    getUsers = () => {
-
-    };
-
     render() {
         return <div className={styles.wrapper}>
-            <button onClick={this.getUsers}>Get Users</button>
             {this.props.users.map(u => <div key={u.id} className={styles.innerWrapper}>
                 <div className={styles.caption}>
                     <div>
