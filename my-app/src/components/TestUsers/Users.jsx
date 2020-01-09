e78684022005970c20d5c3a34194c7dcfad80758
alex8073 <alexandr8073@gmail.com>
2020-01-09 14:33:27 +0300

    dev 55 pagination

--- my-app/src/components/TestUsers/Users.jsx
+++ my-app/src/components/TestUsers/Users.jsx
@@ -6,13 +6,35 @@ import userPhoto from '../../assets/images/user.png';
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&cont=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&cont=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items);
         });
     }
 
     render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div className={styles.wrapper}>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p ? styles.selectedPage : ''}
+                    onClick={ (e) => {this.onPageChanged(p)} }>{p}</span>
+                })}
+            </div>
             {this.props.users.map(u => <div key={u.id} className={styles.innerWrapper}>
                 <div className={styles.caption}>
                     <div>
--- my-app/src/redux/usersReducer.js
+++ my-app/src/redux/usersReducer.js
@@ -1,16 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
-const SET_USERS = 'SET-USERS';
+const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: [
-        // {id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-        //     followed: false, fullName: 'Alex', status: 'boss', location: {city: 'Minsk', country: 'Belarus'}},
-        // {id: 2, photoUrl: 'https://vokrug.tv/pic/person/9/d/5/0/9d5005ea3a93da03696c0c3194be3b8a.jpg',
-        //     followed: true, fullName: 'Ivan', status: 'boss', location: {city: 'Moscow', country: 'Russia'}},
-        // {id: 3, photoUrl: 'https://vokrug.tv/pic/person/6/2/d/c/62dc840fdbc7016571d8bd2031a1215b.jpg',
-        //     followed: false, fullName: 'Elena', status: 'boss', location: {city: 'Kiev', country: 'Ukraine'}}
-    ]
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 2
+
 }
 
 const UsersReducer = (state = initialState, action) => {
@@ -36,14 +35,20 @@ const UsersReducer = (state = initialState, action) => {
                 }),
             };
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: action.users};
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage};
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalUsersCount: action.count};
         default:
             return state;
     }
 };
 
-export const followAC = (userId) => ({type: FOLLOW, userId: userId});
-export const unfollowAC = (userId) => ({type: UNFOLLOW, userId: userId});
-export const setUsersAC =  (users) => ({type: SET_USERS, users: users});
+export const followAC = (userId) => ({type: FOLLOW, userId});
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
+export const setUsersAC = (users) => ({type: SET_USERS, users});
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
 
 export default UsersReducer;
