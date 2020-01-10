b29afbb8d261cb80fab21ea9df253f1d8133b5eb
alex8073 <alexandr8073@gmail.com>
2020-01-10 12:14:15 +0300

    dev 56 Презентационная и Контейнерная компоненты

---
+++ my-app/src/components/TestUsers/Users.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from "./Users.module.css";
+import userPhoto from '../../assets/images/user.png';
+
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+    return <div className={styles.wrapper}>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p ? styles.selectedPage : ''}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}>{p}</span>
+            })}
+        </div>
+        {props.users.map(u => <div key={u.id} className={styles.innerWrapper}>
+            <div className={styles.caption}>
+                <div>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""
+                         className={styles.userPhoto}/>
+                </div>
+                <div>
+                    {u.followed ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
+                </div>
+            </div>
+            <div className={styles.description}>
+                <div>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
+                </div>
+                <div>
+                    <div>{'u.location.country'}</div>
+                    <div>{'u.location.city'}</div>
+                </div>
+            </div>
+        </div>)}
+    </div>
+};
+
+export default Users;
