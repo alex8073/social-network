ac09dd5409fab4d66997fe30f4883dfcf09a5c99
alex8073 <alexandr8073@gmail.com>
2020-02-24 21:18:03 +0300

    dev 80 - инициализация приложения

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import AboutUs from './components/AboutUs/AboutUs';
@@ -6,32 +6,52 @@ import Technologies from './components/Technologies/Technologies';
 import Learning from './components/Learning/Learning';
 import Contacts from './components/Contacts/Contacts';
 import Login from './components/Login/Login';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, withRouter} from 'react-router-dom';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import {connect} from 'react-redux';
+import {compose} from 'redux';
+import {initializeApp} from './redux/appReducer';
+import Preloader from './components/common/Preloader/Preloader';
 
-const App = () => {
-    return (
-        <div>
-            <HeaderContainer/>
-            <Switch>
-                <Route path='/aboutus' component={AboutUs}/>
-                <Route path='/technologies' component={Technologies}/>
-                <Route path='/learning' component={Learning}/>
-                <Route path='/contacts' component={Contacts}/>
-                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                <Route path='/users' render={() => <UsersContainer />}/>
-                <Route path='/login' render={() => <Login />}/>
-                <Route path='/' component={Welcome}/>
-            </Switch>
-        </div>
-    );
-};
+class App extends Component {
+    componentDidMount() {
+        this.props.initializeApp();
+    }
 
-export default App;
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+
+        return (
+            <div>
+                <HeaderContainer/>
+                <Switch>
+                    <Route path='/aboutus' component={AboutUs}/>
+                    <Route path='/technologies' component={Technologies}/>
+                    <Route path='/learning' component={Learning}/>
+                    <Route path='/contacts' component={Contacts}/>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route path='/login' render={() => <Login/>}/>
+                    <Route path='/' component={Welcome}/>
+                </Switch>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+});
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))(App);
 
 
 
--- my-app/src/redux/redux-store.js
+++ my-app/src/redux/redux-store.js
@@ -5,6 +5,7 @@ import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from 'redux-thunk';
 import { reducer as formReducer } from 'redux-form';
+import appReducer from './appReducer';
 
 
 let reducers = combineReducers({
@@ -12,7 +13,8 @@ let reducers = combineReducers({
     DialogsPage: dialogsReduser,
     usersPage: usersReducer,
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    app: appReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
