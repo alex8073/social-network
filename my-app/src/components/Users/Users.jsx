21862abde7080787332497f7f6f4a62a9bee9c4d
alex8073 <alexandr8073@gmail.com>
2020-03-06 22:08:34 +0300

    dev 93 paginator, постраничный вывод

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -6,15 +6,16 @@ import User from './User';
 const Users = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, ...props}) => {
     return <div className={styles.wrapper}>
         <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
-                   totalUsersCount={totalUsersCount} pageSize={pageSize}/>
-        {users.map(u => <User user={u}
-                              key={u.id}
-                              className={styles.innerWrapper}
-                              followingInProgress={props.followingInProgress}
-                              unfollow={props.unfollow}
-                              follow={props.follow}/>
-        )
-        }
+                   totalItemsCount={totalUsersCount} pageSize={pageSize}/>
+        <div className={styles.innerWrapper}>
+            {users.map(u => <User user={u}
+                                  key={u.id}
+                                  className={styles.innerWrapper}
+                                  followingInProgress={props.followingInProgress}
+                                  unfollow={props.unfollow}
+                                  follow={props.follow}/>
+            )
+            }</div>
     </div>
 };
 
