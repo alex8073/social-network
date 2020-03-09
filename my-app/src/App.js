bd1baa65604c6bd2f7ac1db1af3f73b6714666f6
alex8073 <alexandr8073@gmail.com>
2020-03-09 21:08:25 +0300

    dev 94 React.lazy и React.Suspense, зачем?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import AboutUs from './components/AboutUs/AboutUs';
@@ -7,15 +7,17 @@ import Learning from './components/Learning/Learning';
 import Contacts from './components/Contacts/Contacts';
 import Login from './components/Login/Login';
 import {BrowserRouter, Route, Switch, withRouter} from 'react-router-dom';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import {connect, Provider} from 'react-redux';
 import {compose} from 'redux';
 import {initializeApp} from './redux/appReducer';
 import Preloader from './components/common/Preloader/Preloader';
 import store from './redux/redux-store';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer')); // Lazy-loaded
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer')); // Lazy-loaded
+
 
 class App extends Component {
     componentDidMount() {
@@ -30,17 +32,19 @@ class App extends Component {
         return (
             <div>
                 <HeaderContainer/>
-                <Switch>
-                    <Route path='/aboutus' component={AboutUs}/>
-                    <Route path='/technologies' component={Technologies}/>
-                    <Route path='/learning' component={Learning}/>
-                    <Route path='/contacts' component={Contacts}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/login' render={() => <Login/>}/>
-                    <Route path='/' component={Welcome}/>
-                </Switch>
+                <Suspense fallback={<Preloader/>}>
+                    <Switch>
+                        <Route path='/aboutus' component={AboutUs}/>
+                        <Route path='/technologies' component={Technologies}/>
+                        <Route path='/learning' component={Learning}/>
+                        <Route path='/contacts' component={Contacts}/>
+                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                        <Route path='/users' render={() => <UsersContainer/>}/>
+                        <Route path='/login' render={() => <Login/>}/>
+                        <Route path='/' component={Welcome}/>
+                    </Switch>
+                </Suspense>
             </div>
         );
     }
