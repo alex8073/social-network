fefca19d54e28e79ebdb2ae01ce6222276a78f9d
alex8073 <alexandr8073@gmail.com>
2020-03-11 23:35:28 +0300

    Updates

---
+++ precache-manifest.8ca96640f2c299b5aee07e63f8bb577b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d276cfda4840da0eb4a75946437c2ad",
+    "url": "/demo_socialnetwork/index.html"
+  },
+  {
+    "revision": "6bc01b6b7634f5947d10",
+    "url": "/demo_socialnetwork/static/css/3.d7d67821.chunk.css"
+  },
+  {
+    "revision": "5d27f4971d4dc4cb90a0",
+    "url": "/demo_socialnetwork/static/css/4.b2f0c652.chunk.css"
+  },
+  {
+    "revision": "eb833207123d31dc1110",
+    "url": "/demo_socialnetwork/static/css/main.75215cfd.chunk.css"
+  },
+  {
+    "revision": "458f8384b83d55d20536",
+    "url": "/demo_socialnetwork/static/js/2.9f4f8948.chunk.js"
+  },
+  {
+    "revision": "6019a2db51c90e97b4859c840427a0d6",
+    "url": "/demo_socialnetwork/static/js/2.9f4f8948.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bc01b6b7634f5947d10",
+    "url": "/demo_socialnetwork/static/js/3.51f016a2.chunk.js"
+  },
+  {
+    "revision": "5d27f4971d4dc4cb90a0",
+    "url": "/demo_socialnetwork/static/js/4.a575e803.chunk.js"
+  },
+  {
+    "revision": "eb833207123d31dc1110",
+    "url": "/demo_socialnetwork/static/js/main.50aa89f5.chunk.js"
+  },
+  {
+    "revision": "defc3ba403a8756321c7",
+    "url": "/demo_socialnetwork/static/js/runtime-main.b995aae6.js"
+  },
+  {
+    "revision": "b82722facdbf766511439c67032eb545",
+    "url": "/demo_socialnetwork/static/media/bg_main_1.b82722fa.jpg"
+  },
+  {
+    "revision": "8602117ea71992a1ccc2fddfac144864",
+    "url": "/demo_socialnetwork/static/media/icon_android.8602117e.svg"
+  },
+  {
+    "revision": "bc010650edda539ee1172a8635a2f14c",
+    "url": "/demo_socialnetwork/static/media/icon_apple.bc010650.svg"
+  },
+  {
+    "revision": "bf128420dfa6fa4eb23b5a58ae28fa44",
+    "url": "/demo_socialnetwork/static/media/icon_linux.bf128420.svg"
+  },
+  {
+    "revision": "dafa1c75b445466e1e341bd42da7df08",
+    "url": "/demo_socialnetwork/static/media/icon_windows.dafa1c75.svg"
+  },
+  {
+    "revision": "c1a4c655cd1622269c942c3c3a933580",
+    "url": "/demo_socialnetwork/static/media/preloader.c1a4c655.gif"
+  },
+  {
+    "revision": "d7e22ac202e6e1c850f7cd21791f1b2c",
+    "url": "/demo_socialnetwork/static/media/samurai-08.d7e22ac2.jpg"
+  }
+]);
